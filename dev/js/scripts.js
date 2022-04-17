781049ab52b3a42c158074dff5ef63ef8eeebb20
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-04-17 14:09:07 -0400

    animating the hero and logo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,11 @@
-// import { gsap } from "gsap";
+ import { gsap } from "gsap";
+
+ const mainTL= gsap.timeline();
+
+ mainTL
+ .from("#header aside", {duration:4, x:-200, alpha:0}, "part1")
+ .from("#hero ", {duration:4, yPercent:50, alpha:0}, "part1")
+ .from("#hero h2", {duration:1, y:200, alpha:0},"herotext")
+ .from("#hero h1", {duration:1, y:"+=200", alpha:0}, "herotext-=1")
+
+;
