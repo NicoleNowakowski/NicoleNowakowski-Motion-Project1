f0c25be68c27cb801eb41c0df310a24688a74555
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-04-19 16:35:52 -0400

    fixing up timing

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,14 +3,14 @@
  const mainTL= gsap.timeline();
 
  mainTL
- .from("#header", {duration:3, scaleX:0, transformOrigin:"0 0", alpha:0}, "part1")
- .from("#header aside", {duration:6, x:"-=300", rotation:360, transformOrigin:"0 0", ease: "back.inOut", alpha:0}, "part1")
- .from("header nav", {duration:6, x:"+=200", ease: "back.inOut", alpha:0 }, "part1+=1")
- .from("#hero", {duration:3, yPercent:-50, alpha:0}, "part1")
- .from("#hero h2", {duration:3, y:"+=200", alpha:0},"part1+=6")
- .from("#hero h1", {duration:3, y:"+=200", alpha:0}, "part1+=5")
- .from("#content h1", {duration:2,y:"+=200", alpha:0}, "part1+=8")
- .from("#content p", {duration:2,y:"+=200", alpha:0}, "part1+=9")
+ .from("#header", {duration:3, scaleX:0, transformOrigin:"0 0", alpha:0}, "hero")
+ .from("#header aside", {duration:6, x:"-=300", rotation:360, transformOrigin:"0 0", ease: "back.inOut", alpha:0}, "hero")
+ .from("header nav", {duration:6, x:"+=200", ease: "back.inOut", alpha:0 }, "hero+=1")
+ .from("#hero", {duration:3, yPercent:-50, alpha:0}, "hero")
+ .from("#hero h2", {duration:2, y:"+=200", alpha:0},"hero+=6")
+ .from("#hero h1", {duration:2, y:"+=200", alpha:0}, "hero+=5")
+ .from("#content h1", {duration:2,y:"+=200", alpha:0}, "hero+=7")
+ .from("#content p", {duration:2,y:"+=200", alpha:0}, "hero+=8")
  
 
 
