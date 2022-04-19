ddd84d33bb9409d832c9a5bfb943f0e36ccfe3a2
NicoleNowakowski <81652088+NicoleNowakowski@users.noreply.github.com>
2022-04-19 17:12:25 -0400

    beautifying everything

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,15 +12,4 @@
  .from("#content h1", {duration:2,y:"+=200", alpha:0}, "hero+=7")
  .from("#content p", {duration:2,y:"+=200", alpha:0}, "hero+=8")
  
-
-
-
-//.from("#header", {duration:3, scaleX:0, transformOrigin:"0 0", alpha:0}, "part1+=4")
-//.from("#header aside", {duration:6, x:"-=300", rotation:360, transformOrigin:"0 0", ease: "back.inOut", alpha:0}, "part1+=3")
-//.from("header nav", {duration:6, x:"+=200", ease: "back.inOut", alpha:0 }, "part1+=3")
-//.from("#hero", {duration:4, yPercent:-50, alpha:0}, "part1")
-//.from("#hero h2", {duration:2, y:200, alpha:0},"part1+=2")
-//.from("#hero h1", {duration:2, y:"+=200", alpha:0}, "part1+=3")
-//.from("#content h1", {duration:2,y:"+=200", alpha:0}, "part1+=7")
-//.from("#content p", {duration:2,y:"+=200", alpha:0}, "part1+=8")
 ;
